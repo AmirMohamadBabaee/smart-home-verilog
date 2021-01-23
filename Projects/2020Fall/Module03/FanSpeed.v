aed936a3d38fbcfe31141ca76369d43640c01276
Amir01 <iran.volleyball79@gmail.com>
2021-01-23 16:30:01 +0330

    Complete Fanspeed Module

--- Projects/2020Fall/Module03/FanSpeed.v
+++ Projects/2020Fall/Module03/FanSpeed.v
@@ -19,15 +19,32 @@
 ---  Description: Module3: 
 -----------------------------------------------------------*/
 `timescale 1 ns/1 ns
+`include "./CoolHeatSystemUtils.v"
 
 module FanSpeed (
 	input        arst     , // reset [asynch]  
 	input        clk      , // clock [posedge] 
 	input  [7:0] speed    , // speed [duty-cycle]  
-	output       pwm_data   // data  [output]
+	output reg   pwm_data   // data  [output]
 );
 
 	/* write your code here */
+	wire [7:0] counter;
+	
+	always @(posedge clk or negedge arst)
+		begin
+			if (!arst)
+				pwm_data = 1'b0;
+			else if (speed > counter)
+				pwm_data = 1'b1;
+			else
+				pwm_data = 1'b0;
+		end
+	Counter_8bit counter_1(
+		.arst(arst),
+		.clk(clk),
+		.counter_output(counter)
+	);
 	
 	/* write your code here */
 

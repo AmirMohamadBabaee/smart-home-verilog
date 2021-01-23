c2258a6d0bc0332aa7daf9bd7a4e232ab2072d15
Amir01 <iran.volleyball79@gmail.com>
2021-01-23 15:18:44 +0330

    Complete ModePower module

--- Projects/2020Fall/Module03/ModePower.v
+++ Projects/2020Fall/Module03/ModePower.v
@@ -21,12 +21,24 @@
 `timescale 1 ns/1 ns
 
 module ModePower (
-	input  [7:0] chs_conf  , // degree [temprature] 
-	output [3:0] chs_power , // power  [cooler/heater] 
-	output       chs_mode    // model  [heat=1/cool=0]
+	input  [7:0] chs_conf  , 		// degree [temprature] 
+	output reg [3:0] chs_power , 	// power  [cooler/heater] 
+	output reg   chs_mode    	  	// model  [heat=1/cool=0]
 );
 
 	/* write your code here */
+	always @(chs_conf)
+	  begin
+			chs_power = chs_conf[0] + chs_conf[1] + chs_conf[2] + chs_conf[3] + chs_conf[4] + chs_conf[5] + chs_conf[6] + chs_conf[7];
+			if (chs_power[0] == 0) 		// chs_power is Even
+				begin
+					chs_mode = 0;
+				end
+			else								// chs_power is Odd
+				begin
+					chs_mode = 1;
+				end
+	  end
 	
 	/* write your code here */
 

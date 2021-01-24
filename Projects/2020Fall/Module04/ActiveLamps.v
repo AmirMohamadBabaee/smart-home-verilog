ac6f380959ad1267f3c6904a5028a8bcca4a9f4a
Amir01 <iran.volleyball79@gmail.com>
2021-01-24 23:08:53 +0330

    Complete ActiveLamp module

--- Projects/2020Fall/Module04/ActiveLamps.v
+++ Projects/2020Fall/Module04/ActiveLamps.v
@@ -23,12 +23,26 @@
 module ActiveLamps (
 	input  [3:0] tcode  , // time code    [table2 time code   ]
 	input  [3:0] ulight , // user light   [light degree mode  ]
-    input  [3:0] lenght     , // room length  [square room lenght ]
-	output [3:0] active_lights  // number of active light
+    input  [3:0] length     , // room length  [square room lenght ]
+	output reg [3:0] active_lights  // number of active light
 );
 
 	/* write your code here */
+	parameter MORNING 		= 4'b0001, 
+					AFTERNOON 	= 4'b0010, 
+					EVENING 		= 4'b0100, 
+					NIGHT 		= 4'b1000, 
+					OTHER 		= 4'b0000;
 	
+	always @(tcode or ulight)
+		case (tcode)
+			MORNING: active_lights = 4'b0000;
+			AFTERNOON: active_lights = 4'b0000;
+			EVENING: active_lights = {2'b00, length[3:2]};
+			NIGHT: active_lights = ulight;
+			default: active_lights = 4'b0000;
+		endcase
+			
 	/* write your code here */
 
 endmodule

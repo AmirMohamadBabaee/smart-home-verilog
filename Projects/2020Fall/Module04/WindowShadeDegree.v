57b63b09c7a0c5e090dbff2726f6e8a9abdbf1c5
Amir01 <iran.volleyball79@gmail.com>
2021-01-24 23:10:09 +0330

    Complete WindowShadeDegree module

--- Projects/2020Fall/Module04/WindowShadeDegree.v
+++ Projects/2020Fall/Module04/WindowShadeDegree.v
@@ -23,11 +23,24 @@
 module WindowShadeDegree (
 	input  [3:0] tcode  , // time code    [table2 time code   ]
 	input  [3:0] ulight , // user light   [light degree mode  ]
-	output [3:0] wshade   // shade level  [window shade level ]
+	output reg [3:0] wshade   // shade level  [window shade level ]
 );
 
 	/* write your code here */
-	
+	parameter MORNING 		= 4'b0001, 
+					AFTERNOON 	= 4'b0010, 
+					EVENING 	= 4'b0100, 
+					NIGHT 		= 4'b1000, 
+					OTHER 		= 4'b0000;
+					
+	always @(tcode or ulight)
+		case (tcode)
+			MORNING: wshade = 4'b1111;
+			AFTERNOON: wshade = 4'b1100;
+			EVENING: wshade = ulight;
+			NIGHT: wshade = 4'b0000;
+			default: wshade = 4'b0000;
+		endcase
 	/* write your code here */
 
 endmodule

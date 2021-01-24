ddf095c1ea7fcfaa4fc16fdb2193df7c7a14a661
Amir01 <iran.volleyball79@gmail.com>
2021-01-24 23:05:57 +0330

    Complete Decoder4to16_less in LightingSystemUtils file

--- Projects/2020Fall/Module04/LightingSystemUtils.v
+++ Projects/2020Fall/Module04/LightingSystemUtils.v
@@ -23,7 +23,29 @@
 /***********************************************************/
 /************** Design Your Own Modules Below **************/
 
+module Decoder4to16_less (
+	input [3:0] in, 
+	output [15:0] out
+);
 
+	assign out[0]  = in[3] | in[2] | in[1] | in[0];
+	assign out[1]  = in[3] | in[2] | in[1];
+	assign out[2]  = in[3] | in[2] | (in[1] & in[0]);
+    assign out[3]  = in[3] | in[2];
+    assign out[4]  = in[3] | (in[2] & (in[1] | in[0]));
+    assign out[5]  = in[3] | (in[2] & in[1]);
+    assign out[6]  = in[3] | (in[2] & in[1] & in[0]);
+    assign out[7]  = in[3];
+    assign out[8]  = in[3] & (in[2] | in[1] | in[0]);
+    assign out[9]  = in[3] & (in[2] | in[1]);
+    assign out[10] = in[3] & (in[2] | (in[1] & in[0]));
+    assign out[11] = in[3] & in[2];
+    assign out[12] = in[3] & in[2] & (in[1] | in[0]);
+    assign out[13] = in[3] & in[2] & in[1];
+    assign out[14] = in[3] & in[2] & in[1] & in[0];
+    assign out[15] = 1'b0;
+	 
+endmodule
 	
 /************** Design Your Own Modules Above **************/
 /***********************************************************/

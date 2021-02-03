63c074a29e0ae5338e71716b05ad2377ae114103
Amir01 <iran.volleyball79@gmail.com>
2021-02-04 02:15:18 +0330

    Complete implementation of D FlipFlop

--- Projects/2020Fall/Module06/MemoryUnitUtils.v
+++ Projects/2020Fall/Module06/MemoryUnitUtils.v
@@ -23,7 +23,21 @@
 /***********************************************************/
 /************** Design Your Own Modules Below **************/
 
+module D_FF(
+	input arst,
+	input clk,
+	input en,
+	input data,
+	output reg out
+);
 
+	always @ (posedge clk or posedge arst)
+		if (arst)
+			out <= 1'b0;
+		else if (en)
+			out <= data;
+
+endmodule
 	
 /************** Design Your Own Modules Above **************/
 /***********************************************************/

1611bfc30696cf9d6ac37591d84c3245a4f1aeff
Amir01 <iran.volleyball79@gmail.com@users.noreply.github.com>
2021-02-05 23:28:41 +0330

    Complete Module7

--- Projects/2020Fall/Module07/ControlUnit.v
+++ Projects/2020Fall/Module07/ControlUnit.v
@@ -44,12 +44,73 @@ module ControlUnit (
 	input  [ 1:0] syskey    , // key  from memoty unit
 	input  [34:0] configin  , // conf from user
 	output [34:0] configout , // conf to memory unit
-	output        write_en  , // conf mem write enable
-	output [ 2:0] dbg_state   // current state (debug)
+	output reg    write_en  , // conf mem write enable
+	output [2:0]  dbg_state   // current state (debug)
 	);
 
 	/* write your code here */
+	reg [2:0] Prstate, Nxtstate;
+	wire fsm_request;
+	wire dataIsPass;
 	
+	assign fsm_request=(~arst&request);
+	assign dbg_state = Prstate;
+	assign configout=configin;
+	PassCheckUnit password_checker(.pass(password), .key(syskey), .equal(dataIsPass));
+	
+	always@(posedge clk)
+		Prstate=Nxtstate;
+		
+		
+	always@(Prstate or fsm_request or dataIsPass or confirm)
+		if(~fsm_request)
+			begin
+				Nxtstate=`STATE_IDLE;
+				Prstate =`STATE_IDLE;
+			end
+		else
+			case (Prstate)
+				`STATE_IDLE: if(fsm_request) Nxtstate=`STATE_ACTIVE; else Nxtstate=`STATE_IDLE;
+				`STATE_ACTIVE: if(confirm) Nxtstate = (dataIsPass?`STATE_REQUEST : `STATE_TRAP); else Nxtstate=`STATE_ACTIVE;
+				`STATE_TRAP:Nxtstate=`STATE_TRAP;
+				`STATE_REQUEST: if(confirm) Nxtstate=`STATE_STORE; else Nxtstate=`STATE_REQUEST;
+				`STATE_STORE: Nxtstate=`STATE_STORE;
+				`STATE_OTHERS: Nxtstate=`STATE_IDLE;
+				default: Nxtstate=`STATE_IDLE;
+			endcase
+			
+		always@(Prstate or fsm_request or confirm)
+			case (Prstate)
+				`STATE_IDLE:
+					begin
+						write_en <= 1'b0;
+					end
+				`STATE_ACTIVE:
+					begin
+						write_en <= 1'b0;
+					end
+				`STATE_TRAP:
+					begin
+						write_en <= 1'b0;
+					end
+				`STATE_REQUEST:
+					begin
+						write_en <= 1'b0;
+					end
+				`STATE_STORE:
+					if(confirm & fsm_request)
+						write_en <= 1'b1;
+					else
+						write_en <= 1'b0;
+				`STATE_OTHERS:
+					begin
+						write_en <= 1'b0;
+					end
+				default:
+					begin
+						write_en <= 1'b0;
+					end
+			endcase
 	/* write your code here */
 
 endmodule
